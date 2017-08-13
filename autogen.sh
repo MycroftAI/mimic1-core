8435ed02fa465bc1b9c666c86aae1c5573d9ab5a
Sergio Oller <sergioller@gmail.com>
2017-08-13 21:54:53 +0200

    Replace autotools with meson

--- autogen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-mkdir -p "m4" "config"
-case `uname` in Darwin*) glibtoolize --copy ;;
-  *) libtoolize --copy ;; esac
-aclocal \
-&& autoheader \
-&& automake --add-missing \
-&& autoconf
--- gen_ctags.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-#clear the file
-> tags
-
-#append to tags file
-find -name '*.c' -exec ctags --append {} +
-find -name '*.cpp' -exec ctags --append {} +
-find -name '*.h' -exec ctags --append {} +
